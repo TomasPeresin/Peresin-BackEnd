3fcb467953663dac341930c6c307d4884563ec5b
Tomas <tomasperesin@gmail.com>
2023-01-15 18:24:35 -0300

    corrigiendo cors en el back

--- src/main/java/com/portfolio/pti/Controller/CEducacion.java
+++ src/main/java/com/portfolio/pti/Controller/CEducacion.java
@@ -26,7 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendpti.web.app/")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
