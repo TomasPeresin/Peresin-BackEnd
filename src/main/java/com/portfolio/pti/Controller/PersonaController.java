523bd1d806d7cc0f656830310551a60de8d2e84f
Tomas <tomasperesin@gmail.com>
2023-01-15 19:20:46 -0300

    aver

--- src/main/java/com/portfolio/pti/Controller/PersonaController.java
+++ src/main/java/com/portfolio/pti/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontendpti.web.app")
+@CrossOrigin(origins = "http://frontendpti.web.app/")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
